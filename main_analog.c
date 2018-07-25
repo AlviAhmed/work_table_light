c417b5861345f3861022f9e1fa38a41cb9a8e2f2
AlviAhmed <alviahmed1234@gmail.com>
2018-07-24 20:02:23 -0400

    using interrupts, still having trouble with led jumping up and down to low and dim brightness, schematic included

--- main_analog.c
+++ main_analog.c
@@ -16,12 +16,11 @@ int main (void){
 	init(); 
 	ADCinit();
 	while (1){  
-		ADCSRA |= (1 << ADSC); //enabling the adc  
-		while (ADCSRA & (1 << ADSC)); /*looping till convo, ends 
-		can also use an interrerupt for this instead and just the adcValue = ADC in the ISR*/
-		adcValue = ADC; 
-		ledValue = (adcValue >> 2); //converting 10bit to 8bit by shifting down 2 bits
-		dutycycle = ledValue; 
+//		while (ADCSRA & (1 << ADSC)); 
+/*looping till convo, ends can also use an interrerupt for this instead and just the adcValue = ADC in the ISR*/
+//		adcValue = ADC; 
+//		ledValue = (adcValue >> 2); //converting 10bit to 8bit by shifting down 2 bits
+		 
 		
 	}  
 } 
@@ -29,28 +28,35 @@ int main (void){
 void init () { 
 	DDRA &= ~ (_BV(PA1)); //setting this pin as an input 
 	DDRA |= (_BV(PA5)); //the OC1B  
-	TCCR1A |= (1 << COM1B1) | (1 << WGM10); //non inverting mode, fast pwm 8-bit  
-	TCCR1B |= (1 << WGM12);  //8bit fast pwm
-	TIMSK1 |= (1 << TOIE1); //overflow interrupt
+	TCCR1A |= (1 << COM1B1) | (1 << WGM10) | (1 << WGM11); //non inverting mode, fast pwm 10-bit  
+	TCCR1B |= (1 << WGM12);  //10bit fast pwm
+//	TIMSK1 |= (1 << TOIE1); //overflow interrupt
 	sei(); 
-	TCCR1B |= (1 << CS10); //no prescaler (1)
+	TCCR1B |= (1 << CS10); //no prescaling (1)
 	
 } 
 
 void ADCinit() { 
 	ADMUX &=~ ((1 << REFS1) | (1 << REFS0));//Vcc as analog voltage reference 
 	ADMUX |= (1 << MUX1); //Using pin ADC1 (PA1)
-	ADCSRA |= (1 << ADPS1)|(1 << ADPS2)|(1 << ADEN); /*Prescaler of 8, enable ADC*/   
-//	ADCSRB &=~ ( (1 << ADTS2) | (1 << ADTS1) | (1 << ADTS0) );//free running mode
-	DIDR0 |= (1 << ADC1D); /*Disables the digital buffer of the analog pin being used (ADC1D), 
-	reduces power consumption*/
+	ADCSRA |= (1 << ADPS1)|(1 << ADPS2)|(1 << ADEN)|(1 << ADATE) | (1 << ADSC) | (1 << ADIE); /*Prescaler of 8, enable ADC*/     
+	sei();
+//	ADCSRB |= (1 << ADLAR); //left adjust bits
+	ADCSRB &=~ ( (1 << ADTS2) | (1 << ADTS1) | (1 << ADTS0) );//free running mode
+//	DIDR0 |= (1 << ADC1D); /*Disables the digital buffer of the analog pin being used (ADC1D), 
+//	reduces power consumption*/ 
+	ADCSRA |= (1 << ADSC); //start convo
 
 }  
 
+ISR(ADC_vect){ 
+	OCR1B  = ADC;
+} 
+/*
 ISR(TIM1_OVF_vect){ 
 
 OCR1B = dutycycle;
 
 }
 
-
+*/
