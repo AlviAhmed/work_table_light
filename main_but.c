fd405e71987a8d7486ae20228e6c7ebe911091e3
AlviAhmed <alviahmed1234@gmail.com>
2018-08-16 19:12:54 -0400

    Two buttons working now

--- main_but.c
+++ main_but.c
@@ -7,8 +7,10 @@
 
 
 #define colpress (~PINB) & (_BV(0)) //PB0 
+#define partpress (~PINB) & (_BV(1)) //PB1 
 
-int colbut = 0; //last button press for red button (PB0)  
+int colbut = 0; //last button press for red button (PB0)   
+int partbut = 0;//last button press for part button (PB1)
 
 int main (void) {   
 	init(); 
@@ -20,29 +22,34 @@ int main (void) {
 } 
 
 void init() { 
-DDRB &= ~ (1 << PB0);   //enabling input on button 
-PORTB |= (1 << PB0); //tunring on internal resistor
-DDRA |= (1 << PA3); //enabling output of led 
-PORTA |=  (1 << PA3);//initially on
+DDRB &= ~ (1 << PB0) | (1 << PB1);   //enabling input on button 
+PORTB |= (1 << PB0) | (1 << PB1); //tunring on internal resistor
+DDRA |= (1 << PA3) | (1 << PA2); //enabling output of led 
+PORTA &=~  (1 << PA3) | (1 << PA2);//initially off
 }
 
 void pinint() { 
 	GIMSK |= (1 << PCIE1); //enabling pin change interrupt
-	PCMSK1 |= (1 << PB0); //specifying the pins that cause interrupt 
+	PCMSK1 |= (1 << PB0) | (1 << PB1); //specifying the pins that cause interrupt 
 	sei();
 } 
 
 ISR (PCINT1_vect){ //PCINT1 takes care of pins PCINT11:8 
-	_delay_ms(5);  
-	if (colpress){//turning on first led 
-		if (colbut == 0){ 
-			PORTA ^= (1 << PA3); //turns on led
-			colbut = 1; 
-		} 
-		
+	_delay_ms(10);   
+	if (partpress && (partbut == 0)){//turning on first led 
+		PORTA ^= (1 << PA2); //turns on led
+		partbut = 1; 
+	}  
+	else{  
+		partbut = 0; 
+	} 
+	
+	if (colpress && (colbut == 0)){//turning on first led 
+		PORTA ^= (1 << PA3); //turns on led
+		colbut = 1; 
 	}  
 	else{  
 		colbut = 0; 
-	} 
-	
+	}   
+
 }
