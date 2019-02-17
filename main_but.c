b76acbd1dfead08f6e734dd54b0975c97b209e39
AlviAhmed <alviahmed1234@gmail.com>
2019-02-16 19:23:10 -0500

    refined the code, using this version, since I need state machine, got rid of default button

--- main_but.c
+++ main_but.c
@@ -7,10 +7,8 @@
 
 
 #define redpress (~PINB) & (_BV(0)) //PB0 
-#define partpress (~PINB) & (_BV(1)) //PB1  
-#define redread PINA & (_BV(1)) //PA1 
-#define partread PINA & (_BV(0))//PA0
-int byte_val = 0;
+#define partpress (~PINB) & (_BV(1)) //PB1 
+
 int redbut = 0; //last button press for red button (PB0)    
 int redenable = 0;
 int partbut = 0;//last button press for part button (PB1)
@@ -19,11 +17,19 @@ int potenable = 1;
 int main (void) {   
 	init(); 
 	pinint();
-	while (1){       
-
-
-	}  
-
+	while (1){    
+		_delay_us(1);
+		if (partenable == 1){ 
+			PORTA |= (1 << PA0);   
+		}else { 
+			PORTA &=~ (1 << PA0);  
+		}   
+		if (redenable == 1){ 
+			PORTA |= (1 << PA1);   
+		}else { 
+			PORTA &=~ (1 << PA1); 
+		}   
+	} 
 	return (0);
 
 } 
@@ -31,63 +37,41 @@ int main (void) {
 void init() { 
 DDRB &= ~ (1 << PB0) | (1 << PB1);   //enabling input on button 
 PORTB |= (1 << PB0) | (1 << PB1); //tunring on internal resistor
-DDRA |= (1 << PA1) | (1 << PA0) | (1 << PA2); //enabling output of led 
-PORTA &=~ 0x07 ;//initially off 
-/*Party button = PA0 
-  Red button = PA1 
-  Default state button = PA2 */
-} 
-
+DDRA |= (1 << PA0) | (1 << PA1) | (1 << PA2); //enabling output of led 
+PORTA &=~  (1 << PA0) | (1 << PA1) | (1 << PA2);//initially off
+}
 
 void pinint() { 
 	GIMSK |= (1 << PCIE1); //enabling pin change interrupt
 	PCMSK1 |= (1 << PB0) | (1 << PB1); //specifying the pins that cause interrupt 
 	sei();
-}  
-
-void default_state() {
-
-}
+} 
 
 ISR (PCINT1_vect){ //PCINT1 takes care of pins PCINT11:8 
 	_delay_ms(5);   
-	if (partpress &&  partbut == 0 ){           
-		if (PORTA == 0x02){
-			PORTA = (PORTA & ~0x02) | 0x01;  
-		}  
-		else {  
-			PORTA ^= 0x01; 
-		}
+	if (partpress &&  partbut == 0 ){      
+		redenable = 0; 
+		partenable = !partenable;//toggling value instead of LED 
 		partbut = 1;  
 	}   
 	else {  
-		partbut = 0;	  
+		partbut = 0;	 
+		
 	}
-	if (redpress && redbut == 0){       
-		if (PORTA == 0x01){ 
-			PORTA = (PORTA & ~0x01) | 0x02;
-		}  
-		else {  
-			PORTA ^= 0x02; 
-		}
+	if (redpress && redbut == 0){    
+		partenable = 0;	 
+		redenable = !redenable;
 		redbut = 1; 
 	}  
 	else{   
 		redbut = 0; 
-	}            
-
-
-	/*
+	}    
 	if (partpress && redpress && partbut == 0 && redbut == 0){//if both buttons pressed, default to one of them  
-		PORTA ^= 0x04;
-	
 		redenable = 0;
-		partenable = !partenable;  
-
+		partenable = !partenable; 
 		redbut = 1; 
 	}else { 
 		redbut = 0;
-	}  
-	*/
+	} 
 
 }
